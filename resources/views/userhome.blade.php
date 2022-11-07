c71c9a9620148729d9a28b2bfb63d64a8dd421f1
Bandfal <naufalgame37@gmail.com>
2022-11-07 09:49:12 +0700

    penggantian beberapa file
    test

---
+++ resources/views/userhome.blade.php
@@ -0,0 +1,77 @@
+@extends('layout.usertemp-blog')
+
+@push('css')
+  <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+@endpush
+
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Artikel Model</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/home">Home</a></li>
+              <li class="breadcrumb-item active">Artikel Model</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Atque ipsam asperiores, ducimus pariatur neque placeat ex iusto eligendi quas tempore blanditiis, dicta voluptate. Voluptatum enim ex esse totam, distinctio odio.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui earum aliquam exercitationem explicabo corporis cum rem ad, est magni soluta ipsam dicta nisi in repudiandae illum labore nulla, odio praesentium.</p>
+          </div>
+        </div>
+      </a>
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Obcaecati esse quibusdam quod consequuntur ipsum in, magni tenetur distinctio perspiciatis corrupti, rerum voluptatibus adipisci labore totam molestiae quisquam dolorum error impedit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea repellendus obcaecati quas laudantium dolore eligendi culpa ratione quam labore libero quasi iste sunt, exercitationem excepturi impedit non numquam fuga debitis.</p>
+          </div>
+        </div>
+      </a>
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Quaerat quasi eaque sequi laboriosam, aliquid fugiat recusandae repellendus, quidem maiores accusantium voluptatem non cum, repellat eum consectetur facere officiis? Blanditiis, perferendis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ratione aperiam? Odit fuga incidunt facere explicabo quidem, voluptate voluptatem accusantium ipsum reiciendis repellat, corrupti, error aliquam. Consequuntur voluptatem aut corporis!</p>
+          </div>
+        </div>
+      </a>
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Atque ipsam asperiores, ducimus pariatur neque placeat ex iusto eligendi quas tempore blanditiis, dicta voluptate. Voluptatum enim ex esse totam, distinctio odio.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui earum aliquam exercitationem explicabo corporis cum rem ad, est magni soluta ipsam dicta nisi in repudiandae illum labore nulla, odio praesentium.</p>
+          </div>
+        </div>
+      </a>
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Obcaecati esse quibusdam quod consequuntur ipsum in, magni tenetur distinctio perspiciatis corrupti, rerum voluptatibus adipisci labore totam molestiae quisquam dolorum error impedit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea repellendus obcaecati quas laudantium dolore eligendi culpa ratione quam labore libero quasi iste sunt, exercitationem excepturi impedit non numquam fuga debitis.</p>
+          </div>
+        </div>
+      </a>
+      <a href="">
+        <div class="card text-bg-primary">
+          <h4 class="card-header"><p class="card-title">Lorem ipsum</p></h4>
+          <div class="card-body"><p class="card-text">dolor sit amet consectetur adipisicing elit. Quaerat quasi eaque sequi laboriosam, aliquid fugiat recusandae repellendus, quidem maiores accusantium voluptatem non cum, repellat eum consectetur facere officiis? Blanditiis, perferendis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ratione aperiam? Odit fuga incidunt facere explicabo quidem, voluptate voluptatem accusantium ipsum reiciendis repellat, corrupti, error aliquam. Consequuntur voluptatem aut corporis!</p>
+          </div>
+        </div>
+      </a>
+    {{-- make a html blog content with the css style--}}
+</div>
+@endsection
